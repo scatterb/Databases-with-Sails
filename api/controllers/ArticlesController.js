27073b09022c6099c5859dd37541d19a730f061e
scatterb <jussi.ketomaki@edu.turkuamk.fi>
2017-11-15 09:53:48 -0800

    Now able to edit & delete articles

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -28,8 +28,41 @@ module.exports = {
             }
 
             res.redirect('/articles/list');
-        })
+        });
+    },
+    delete: function(req, res){
+        Articles.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Articles.findOne({id:req.params.id}).exec(function(err, article){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.view('edit', {article:article});
+        });
+    },
+    update: function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.update({id: req.params.id},{title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+
+        return false;
     }
-	
 };
 
