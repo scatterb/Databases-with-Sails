9e1f0f0554a759e74e4824b2f377c52a148023d1
scatterb <jussi.ketomaki@edu.turkuamk.fi>
2017-11-15 04:09:15 -0800

    Article list created for article page

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
+    list:function(req, res) {
+        //res.view('list');
+        Articles.find({}).exec(function(err, articles) {
+            if(err) {
+                res.send(500, {error: "Database Error"});
+            }
+            res.view('list', {articles:articles});
+        });
+    }
 	
 };
 
